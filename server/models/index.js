8ee46cb72f51ba21f08b4d5469f2840dbf4f6bae
teradonburi <daikiterai@gmail.com>
2018-05-26 22:19:45 +0900

    temp

--- server/models/index.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-require('fs').readdirSync(__dirname).forEach(e => {
-  const name = /^([a-z]+)\.js$/i.test(e) && RegExp.$1
-  if (name && name !== 'index') {
-    const model = require('./' + name)
-    module.exports[model.modelName] = model
-  }
-})

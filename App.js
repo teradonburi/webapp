0752c8e2c55d12b7877234fd0b98703c41a624a6
teradonburi <daikiterai@gmail.com>
2018-06-22 22:22:41 +0900

    fix

--- App.js
+++ App.js
@@ -1,14 +1,45 @@
 import React from 'react'
 
-export default class MainPage extends React.Component {
-  state = {}
+class Test extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { a: 'abc'}
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    // console.log('----this.props----', this.props)
+    console.log('----nextProps----', nextProps)
+    console.log('----prevState----', prevState)
+    return prevState
+  }
 
   componentDidMount () {}
+
+  render () {
+    console.log('----this.state----', this.state)
+    return <div/>
+  }
+}
+
+const arr = ['a', 'b', 'c']
+
+export default class MainPage extends React.Component {
+  state = {
+    idx: 0,
+  }
+
+  buttonClick = () => {
+    const { idx } = this.state
+    this.setState({idx: (idx + 1) % arr.length})
+  }
 
   render () {
 
     return (
       <div>
+        <button onClick={this.buttonClick}>Button</button>
+        <Test myprop={arr} idx={this.state.idx} />
       </div>
     )
   }
